a8db93696e3b67511959d155f180f41f7efb0b05
Syed Muhammad Hamza Bukhari <59118094+SMHamzaBukhari@users.noreply.github.com>
2022-04-14 22:16:59 +0500

    Add files via upload

---
+++ lab4/app.js
@@ -0,0 +1,75 @@
+// =================================Task 1
+// var num1 = +prompt("Enter first num; ");
+// var num2 = +prompt("Enter Second num: ");
+// var op = prompt("Enter Operator: ");
+// if(op == '+'){
+//     alert(num1 + num2);
+// }else if(op == '-'){
+//         alert(num1 - num2);
+// }else if(op =='/'){
+//     alert(num1 / num2);
+    
+// }else if( op=='*'){
+//     alert(num1 * num2);     
+// }else{
+//     alert("invalid input!");
+// }
+            
+// // =================================Task 2
+
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+// var a= [...array];
+// var b= [...array];
+// console.log(array);
+// // // ========= 1.
+// console.log("Task 1:");
+// array.shift();
+// console.log(array);
+
+// // ========= 2.
+// console.log("task 2:");
+// array.sort();
+// console.log(array);
+
+// // ========= 3.
+// console.log("task 3:");
+// array.push("Kiwi");
+// console.log(array);
+
+// // ========= 4.
+// console.log("task 4:");
+// console.log(a)
+// a.splice(1,1)
+// console.log(a);
+
+// // ========= 5.
+// console.log("task 4:");
+// console.log(b)
+// b.reverse()
+// console.log(b);
+
+// =================================Task 3
+// let alarm = () =>{
+//        inp = prompt("Alarm is Ringing. Press 'S' to snooze and 'D' to dismiss");
+// }
+// alarm();
+// if(inp == "S" || inp == "s"){
+// alarm();
+// }
+// else if(inp == "D" || inp == "d"){
+//    prompt("Good Morning");
+// }else{
+//     prompt("wrong input");
+    
+// }
+
+// =================================Task 4
+let obj = {
+    username: "Hamza",
+    password: "12345"
+}
+console.log(obj);
+let database = [obj];
+console.log(database);
+let newsfeed = [{username:"",timeline:""},{username:"",timeline:""},{username:"",timeline:""}];
+console.log(newsfeed);
